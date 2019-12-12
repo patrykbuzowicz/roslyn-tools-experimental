9b7de75bbe0d9b3465cfbf0a32adec63c2aa36ea
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2019-12-12 22:29:58 +0100

    Add project files.

---
+++ Roslyn/ExtractClassesWalker.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace Roslyn
+{
+    public class ExtractClassesWalker : CSharpSyntaxWalker
+    {
+        public List<ClassDeclarationSyntax> Classes { get; } = new List<ClassDeclarationSyntax>();
+
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            Classes.Add(node);
+            base.VisitClassDeclaration(node);
+        }
+    }
+}
---
+++ Roslyn/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roslyn
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var workspace = new AdhocWorkspace();
+            var solution = workspace.CurrentSolution;
+            var project = solution.AddProject("project", "assembly", LanguageNames.CSharp);
+            var document = project.AddDocument("file.cs", @"
+namespace Main.Proj 
+{
+    class A {}
+
+    [Test]
+    class B {
+        public int Number {get;set;}
+    }
+}
+");
+
+            var syntaxTree = await document.GetSyntaxTreeAsync();
+            var compUnit = syntaxTree.GetCompilationUnitRoot();
+
+            var visitor = new ExtractClassesWalker();
+            compUnit.Accept(visitor);
+
+            var docs = visitor.Classes
+                .Where(ParentIsNamespace)
+                .Select(WrapWithParentNamespace)
+                .Select(x => x.ToFullString())
+                .ToList();
+
+            docs.ForEach(x => Console.WriteLine(x));
+        }
+
+        private static bool ParentIsNamespace(ClassDeclarationSyntax arg) => arg.Parent is NamespaceDeclarationSyntax;
+
+        private static NamespaceDeclarationSyntax WrapWithParentNamespace(ClassDeclarationSyntax classDeclaration)
+        {
+            var ns = classDeclaration.Parent as NamespaceDeclarationSyntax;
+            return ns
+                .RemoveNodes(ns.Members, SyntaxRemoveOptions.KeepDirectives)
+                .AddMembers(classDeclaration);
+        }
+    }
+}
